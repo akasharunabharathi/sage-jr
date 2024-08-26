248e1b1aa4aa6ee10f613600ad417dfa165da12f
Akash Arunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2024-08-25 19:16:25 -0500

    Create gradio_setup.py

---
+++ gradio_setup.py
@@ -0,0 +1,16 @@
+import gradio as gr
+from chatbot import gradio_search
+
+iface = gr.Interface(
+    fn=gradio_search, 
+    inputs=gr.Textbox(lines=2, placeholder="type away here"),
+    outputs="text",
+    title="sage jr",
+    description="Looking for someone working on films? SaaS? AI?",
+    examples=[
+        ["Who's working on AI apps?"],
+        ["Find people in data science"],
+    ]
+)
+
+iface.launch(share=True)
