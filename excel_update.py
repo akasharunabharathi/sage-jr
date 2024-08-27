4ee9522b23a59d9423be3cab0b6f79ddf02c2dfe
Akash Arunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2024-08-27 15:47:39 -0500

    Update excel_update.py

--- excel_update.py
+++ excel_update.py
@@ -14,7 +14,6 @@ from decouple import config
 # EXCEL_URL = config("EXCEL_URL")
 # SHEET_ID = config("SHEET_ID")
 SHEET_ID = "1xgBtPU4p6WX2AJYTdQF3ZpV4Q3Q3ynM4yfXRSC1oCCY"
-SERVICE_ACCOUNT_FILE = "/content/sage-jr/token.json"
 RANGE_NAME = "Sheet1!A1:Z1000"
 
 def get_credentials():
@@ -28,7 +27,8 @@ def get_credentials():
         "auth_uri": os.environ.get("AUTH_URI"),
         "token_uri": os.environ.get("TOKEN_URI"),
         "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL")
+        "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL"),
+        "universe_domain": os.environ.get("UNIVERSE_DOMAIN")
     }
     scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
     return Credentials.from_service_account_info(credentials_dict, scopes=scopes)
@@ -52,8 +52,9 @@ def fetch_data_to_csv():
     try:
         df = pd.DataFrame(values[2:], columns=values[1])
         # Convert to CSV
-        csv_filename = f"/content/sage-jr/latest_excel_data.csv"
+        csv_filename = f"latest_excel_data.csv"
         df.to_csv(csv_filename, index=False, encoding='utf-8')
+        print("Conversion complete!\n\n")
     except Exception as e:
         print(f"Error converting to CSV: {e}")
         return None
